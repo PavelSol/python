4605695eca59d8e08287b22c52f217b4a5255b16
PavelSol <110417183+PavelSol@users.noreply.github.com>
2022-08-02 10:33:07 +0300

    second commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ lst = []
 for i in input().split():
     lst1 = input().split()
     for i in range(len(lst1)):
-        lst1[i] = int(lst1[i])
+        lst[i] = int(lst1[i])
     lst.append(lst1)
     if input() == "end":
-        print(*lst)
+        print(lst)
