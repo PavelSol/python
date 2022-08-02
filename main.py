92609c66ab8db7fafbeabc770e14fdd05475128e
PavelSol <110417183+PavelSol@users.noreply.github.com>
2022-08-02 16:05:33 +0300

    Всё

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 mat = []
 lst = input().split()
 lengthi = len(lst)
@@ -11,11 +12,19 @@ while cl == 1:
     else:
         break
 lengthj = len(mat)
+mat2 = copy.deepcopy(mat)
 for i in range(0, lengthj):
     for j in range(0, lengthi):
         k = int(mat[i][j])
-        mat[i][j] = int(mat[i - 1][j]) + int(mat[i + 1][j]) + int(mat[i][j - 1]) + int(mat[i][j + 1])
-for i in range(0, lengthj):         #вывод матрицы
+        if (i < lengthj - 1) and (j < lengthi - 1):
+            mat[i][j] = int(mat2[i - 1][j]) + int(mat2[i + 1][j]) + int(mat2[i][j - 1]) + int(mat2[i][j + 1])
+        elif (i == lengthj - 1) and (j < lengthi - 1):
+            mat[i][j] = int(mat2[i - 1][j]) + int(mat2[0][j]) + int(mat2[i][j - 1]) + int(mat2[i][j + 1])
+        elif (j == lengthi - 1) and (i < lengthj - 1):
+            mat[i][j] = int(mat2[i - 1][j]) + int(mat2[i + 1][j]) + int(mat2[i][j - 1]) + int(mat2[i][0])
+        elif (i == lengthj - 1) and (j == lengthi - 1):
+            mat[i][j] = int(mat2[i - 1][j]) + int(mat2[0][j]) + int(mat2[i][j - 1]) + int(mat2[i][0])
+for i in range(0, lengthj):
     print(*mat[i])
 
 
